67fc073fb4b1c6b47d1f71a1c222704ffae6fa60
Denys Karpenko <denisdataforsep@gmail.com>
2023-07-25 21:05:30 -0700

    try own midleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,15 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
 	"github.com/deniskarpenko/go-web-app/pkg/config"
 	"github.com/deniskarpenko/go-web-app/pkg/handlers"
+	"github.com/go-chi/chi/v5"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	//mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
