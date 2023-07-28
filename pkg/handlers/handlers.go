c57fbe1e0f4891b2b64a91981fe27739da31b9ba
Denys Karpenko <denisdataforsep@gmail.com>
2023-07-27 21:05:41 -0700

    add session

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,14 +29,20 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
-func (m *Repository) About(w http.ResponseWriter, gor *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	//perform some logic
 	stringMap := make((map[string]string))
 	stringMap["test"] = "Hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
 
 	//send data to the template
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
